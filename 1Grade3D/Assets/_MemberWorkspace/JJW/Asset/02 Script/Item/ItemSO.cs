e6061b9861821f9aa084b4d4ee3f8ed0e576da7f
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-07-23 16:10:43 +0900

    ItemSO 수정

--- 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Item/ItemSO.cs
+++ 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Item/ItemSO.cs
@@ -12,7 +12,8 @@ namespace _MemberWorkspace.JJW.Asset._02_Script.Item
         [field: SerializeField] public Sprite Icon { get; private set; }
         [field: SerializeField] public ItemGrade Grade { get; private set; }
         [field: SerializeField] public int MaxDurability { get; private set; }//시작 내구도 체력
-        [field: SerializeField] public float Weight { get; private set; }
-        [field: SerializeField] public int Price { get; private set; }
+        [Range(1f, 100f)][field: SerializeField] public float Weight { get; private set; }
+        [Range(1, 1000)][field: SerializeField] public int Price { get; private set; }
+        [Range(1f, 5f)][field: SerializeField] public float Scale { get; private set; }
     }
 }
