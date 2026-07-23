5467e16de684e53f4f745300f97184daa73b2797
rgnjjw <rgn7jjw@gmail.com>
2026-07-23 16:36:34 +0900

    Complate

---
+++ 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Events/FlowEvent.cs
@@ -0,0 +1,9 @@
+using GameLib.EventChannelSystem;
+
+namespace _MemberWorkspace.JJW.Asset._02_Script.Events
+{
+    public class ExploreStartEvent : GameEvent { }
+    public class ExploreEndEvent : GameEvent { }
+    public class StormStartEvent : GameEvent { }
+    public class StormEndEvent : GameEvent { }
+}
--- 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Events/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-using GameLib.EventChannelSystem;
-
-namespace _MemberWorkspace.JJW.Asset._02_Script.Events
-{
-    public class ExploreStartEvent : GameEvent { }
-    public class ExploreEndEvent : GameEvent { }
-    public class StormStartEvent : GameEvent { }
-    public class StormEndEvent : GameEvent { }
-}
