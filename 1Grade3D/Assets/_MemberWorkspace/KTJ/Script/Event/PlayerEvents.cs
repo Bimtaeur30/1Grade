d60edce29d4aadc4b80d5f7a6547b1691ecf341d
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-07-23 17:36:25 +0900

    스탯 업그레이드 이벤트

--- 1Grade3D/Assets/_MemberWorkspace/KTJ/Script/Event/PlayerEvents.cs
+++ 1Grade3D/Assets/_MemberWorkspace/KTJ/Script/Event/PlayerEvents.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public static class PlayerEvents
 {
     public static readonly ItemEquipEvent ItemEquipEvent = new();
+    public static readonly RunSpeedUpgradeEvent RunSpeedUpgradeEvent = new();
+    public static readonly MaxWeightUpgradeEvent MaxWeightUpgradeEvent = new();
 }
 
 public class ItemEquipEvent : GameEvent
@@ -13,6 +15,24 @@ public class ItemEquipEvent : GameEvent
     public ItemEquipEvent Init(ItemSO item)
     {
         Item = item;
+        return this;
+    }
+}
+public class RunSpeedUpgradeEvent : GameEvent
+{
+    public int Amount { get; private set; }
+    public RunSpeedUpgradeEvent Init(int amount)
+    {
+        Amount = amount;
+        return this;
+    }
+}
+public class MaxWeightUpgradeEvent : GameEvent
+{
+    public int Amount { get; private set; }
+    public MaxWeightUpgradeEvent Init(int amount)
+    {
+        Amount = amount;
         return this;
     }
 }
