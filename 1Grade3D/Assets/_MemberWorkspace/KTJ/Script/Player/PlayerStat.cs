a8f903936130b1925936e565f51bb2e99852f035
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-07-23 18:06:17 +0900

    스캔쿨타임 스탯 추가

--- 1Grade3D/Assets/_MemberWorkspace/KTJ/Script/Player/PlayerStat.cs
+++ 1Grade3D/Assets/_MemberWorkspace/KTJ/Script/Player/PlayerStat.cs
@@ -8,13 +8,24 @@ public class PlayerStat : MonoBehaviour
 
     [field: SerializeField] public int MaxWeight { get; private set; }
     [field: SerializeField] public int RunSpeed { get; private set; }
+    [field: SerializeField] public int ScanCooltime { get; private set; }
 
+    public event Action<int> MaxWeightChanged;
     public event Action<int> RunSpeedChanged;
+    public event Action<int> ScanCooltimeChanged;
 
     private void Awake()
     {
         PlayerChannel.AddListener<MaxWeightUpgradeEvent>(HandleMaxWeightUpgradeEvent);
         PlayerChannel.AddListener<RunSpeedUpgradeEvent>(HandleRunSpeedUpgradeEvent);
+        PlayerChannel.AddListener<ScanCooltimeUpgradeEvent>(HandleScanCooltimeUpgradeEvent);
+    }
+
+    private void Start()
+    {
+        RunSpeedChanged?.Invoke(RunSpeed);
+        MaxWeightChanged?.Invoke(MaxWeight);
+        ScanCooltimeChanged?.Invoke(ScanCooltime);
     }
 
     private void HandleRunSpeedUpgradeEvent(RunSpeedUpgradeEvent @event)
@@ -26,5 +37,12 @@ public class PlayerStat : MonoBehaviour
     private void HandleMaxWeightUpgradeEvent(MaxWeightUpgradeEvent @event)
     {
         MaxWeight += @event.Amount;
+        MaxWeightChanged?.Invoke(MaxWeight);
+    }
+
+    private void HandleScanCooltimeUpgradeEvent(ScanCooltimeUpgradeEvent @event)
+    {
+        ScanCooltime -= @event.Amount;
+        ScanCooltimeChanged?.Invoke(ScanCooltime);
     }
 }
