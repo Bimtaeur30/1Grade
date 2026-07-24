101c8d88afb0409f9352bb2b9a624febc4d8546d
rgnjjw <rgn7jjw@gmail.com>
2026-07-24 11:24:23 +0900

    j

--- 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Managers/GameFlowManager.cs
+++ 1Grade3D/Assets/_MemberWorkspace/JJW/Asset/02 Script/Managers/GameFlowManager.cs
@@ -11,7 +11,7 @@ namespace _MemberWorkspace.JJW.Asset._02_Script.Managers
         public static GameFlowManager Instance { get; private set; }
         public GameState CurrentState { get; private set; }
 
-        [SerializeField] private float exploreTimeLimit = 30f;
+        [SerializeField] private float exploreTimeLimit = 30f;//임시
         [SerializeField] private EventChannelSO eventChannel;
 
         private float _timer;
@@ -34,19 +34,19 @@ namespace _MemberWorkspace.JJW.Asset._02_Script.Managers
             }
         }
 
-        public void EndTurn() //타이머 안끝나도 상점갈수 있으니까 그냥 퍼블릭으로 둠 ㅇㅇ
+        public void EndTurn() //타이머 안끝나도 상점갈수 있으니까 그냥 퍼블릭으로 둠 ㅇㅇ 상점 갈때 호출
         {
             CurrentState = GameState.Shopping;
             eventChannel.RaiseEvent(new TurnEndEvent());
         }
 
-        public void StartStorm()
+        public void StartStorm()//상점에서 아이템 고르면 실행
         {
             CurrentState = GameState.Storm;
             eventChannel.RaiseEvent(new StormStartEvent());
         }
 
-        public void EndStorm()
+        public void EndStorm()//지한이가 호출(폭풍연출 끝나면)
         {
             CurrentState = GameState.Exploring;
             eventChannel.RaiseEvent(new StormEndEvent());
